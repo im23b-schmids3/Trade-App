d9247b05d9a866b6a21df68db286d097ad05a0f3
schmids3 <schmids3@bzz.ch>
2026-06-09 13:55:16 +0200

    #1 please

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/trade/",
+  base: "./",
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5000,
     open: true
+  },
+  preview: {
+    port: 5000
   },
   build: {
     outDir: "dist",
